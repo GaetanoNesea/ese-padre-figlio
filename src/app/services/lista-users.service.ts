8d8367ebfdf699fe299ceb93d37a448e46041b5f
Gaetano Sabino <g.sabino@nesea.it>
2023-02-02 15:13:22 +0100

    creazione service

---
+++ src/app/services/lista-users.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ListaUsersService {
+
+
+  // Posso creare proprietà della stessa
+
+  constructor() { }
+
+  // Posso creare metodi
+  
+}
